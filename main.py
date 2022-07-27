75acf9dde3cf6395dfe89449f8a135b736b6e119
Mehchu <dtmackenzie06@outlook.com>
2022-07-27 19:44:33 +0100

    Fully functioning calculator

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from PyQt6.QtGui import QPalette
+from PyQt6.QtGui import QPalette, QFontMetrics
 from PyQt6.QtWidgets import (QWidget, QPushButton, QApplication,
-                             QGridLayout, QButtonGroup, QLabel)
+                             QGridLayout, QButtonGroup, QLabel, QSizePolicy)
 from PyQt6.QtCore import Qt
 import sys
 
@@ -58,18 +58,24 @@ class Window(QWidget):
     def __init__(self):
         super().__init__()
         self.display = QLabel(self)
+        self.display.setStyleSheet("background-color: lightgray;border: 1px solid black")
+        self.fontD = self.font()
+        self.fontD.setPointSize(20)
+        self.display.setFont(self.fontD)
         self.populate()
 
     def populate(self):
-        self.display.setStyleSheet("background-color: lightgray;border: 1px solid black")
+
         grid = QGridLayout()
-        grid.setRowMinimumHeight(0, 10)
+
         grid.addWidget(self.display, 0, 0, 1, 5)
 
         x = 0
         y = 1
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Minimum)
         for index, character in enumerate(buttonList):
             button = QPushButton(character, self)
+            button.setSizePolicy(sizePolicy)
             buttonGroup.addButton(button, index)
             grid.addWidget(button, y, x)
 
@@ -79,8 +85,13 @@ class Window(QWidget):
                 y += 1
 
         buttonGroup.buttonClicked.connect(textEntered)
-
+        grid.setRowStretch(0, 3)
+        for row in range(grid.rowCount() - 1):
+            row += 1
+            grid.setRowStretch(row, 2)
+        grid.setSpacing(5)
         self.setLayout(grid)
+
         self.setGeometry(300, 300, 200, 200)
         self.setWindowTitle('Calculator')
 
